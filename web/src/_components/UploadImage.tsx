bde720124cdf7d4691135f2aae6ebe938f9bdeae
dhruvkaushik305 <dhruvkaushik305@gmail.com>
2024-09-01 07:11:05 +0530

    added english and hindi translations

--- web/src/_components/UploadImage.tsx
+++ web/src/_components/UploadImage.tsx
@@ -13,88 +13,10 @@ export default function UploadImage() {
   const { t } = useTranslation();
   const [uploading, setUploading] = React.useState(true);
   const [img, setImg] = React.useState("");
-  const [imgPrep, setImgPrep] = React.useState(true);
+  const [imgPrep, setImgPrep] = React.useState(false);
   const [cameraCapture, setCameraCapture] = React.useState(false);
   const webcamRef = React.useRef<Webcam>(null);
   const diseaseName = "Apple__Cedar_apple_rust";
-  const cropData = {
-    Apple__Cedar_apple_rust: {
-      leafName: "Apple",
-      diseaseName: "Cedar Apple Rust",
-      description:
-        "Cedar Apple Rust is a fungal disease that causes bright orange, yellow, or reddish spots on apple leaves, leading to reduced fruit quality and yield.",
-      precautions:
-        "Remove nearby juniper or cedar trees, which host the rust fungus. Apply fungicides in early spring.",
-      treatment:
-        "Apply fungicides at bud break and continue through the growing season as needed. Prune infected leaves and dispose of them properly.",
-      water:
-        "Ensure consistent watering, avoiding overhead irrigation to reduce leaf wetness and prevent the spread of the disease.",
-    },
-    Apple__healthy: {
-      leafName: "Apple",
-      diseaseName: "Healthy",
-      description:
-        "This apple leaf is healthy with no signs of disease or damage, indicating optimal growing conditions and proper care.",
-      precautions:
-        "Maintain regular monitoring and care to prevent potential diseases. Ensure proper watering, fertilization, and pruning.",
-      treatment:
-        "No treatment necessary. Continue regular care and monitoring to keep the plant healthy.",
-      water:
-        "Provide consistent watering according to the plant's needs, avoiding both under-watering and over-watering.",
-    },
-
-    "Corn_(maize)__Common_rust": {
-      leafName: "Corn (maize)",
-      diseaseName: "Common Rust",
-      description:
-        "Common Rust in maize causes small, reddish-brown pustules on leaves, which can reduce photosynthesis and yield if severe.",
-      precautions:
-        "Plant resistant varieties and ensure proper crop rotation. Apply fungicides if the disease pressure is high.",
-      treatment:
-        "Apply fungicides early in the disease development. Remove and destroy infected leaves to reduce spread.",
-      water:
-        "Water at the base of the plant to keep foliage dry, as moisture on leaves can promote rust development.",
-    },
-
-    "Corn_(maize)___Northern_Leaf_Blight": {
-      leafName: "Corn (maize)",
-      diseaseName: "Northern Leaf Blight",
-      description:
-        "Northern Leaf Blight is characterized by long, gray-green, cigar-shaped lesions on leaves, which can lead to significant yield loss.",
-      precautions:
-        "Use resistant hybrids, practice crop rotation, and ensure proper spacing for airflow.",
-      treatment:
-        "Apply fungicides at the first sign of symptoms. Remove infected debris from the field to prevent overwintering of the fungus.",
-      water:
-        "Water at the base and avoid overhead irrigation to minimize leaf wetness, reducing the spread of blight.",
-    },
-
-    "Pepper,bell_Bacterial_spot": {
-      leafName: "Pepper, bell",
-      diseaseName: "Bacterial Spot",
-      description:
-        "Bacterial Spot causes small, dark, water-soaked lesions on leaves and fruit, leading to defoliation and reduced fruit quality.",
-      precautions:
-        "Use disease-free seeds, practice crop rotation, and avoid overhead watering. Apply copper-based bactericides.",
-      treatment:
-        "Remove and destroy infected plants. Apply bactericides at the first sign of disease to control spread.",
-      water:
-        "Water at the base of the plants to prevent moisture from splashing onto the leaves, which can spread bacteria.",
-    },
-
-    "Pepper,_bell__healthy": {
-      leafName: "Pepper, bell",
-      diseaseName: "Healthy",
-      description:
-        "This bell pepper plant is healthy with no visible signs of disease or stress, indicating good growing conditions.",
-      precautions:
-        "Continue regular care, including proper watering, fertilization, and pest monitoring.",
-      treatment:
-        "No treatment necessary. Maintain regular care to keep the plant healthy.",
-      water:
-        "Ensure consistent watering, allowing the soil to dry slightly between waterings to avoid root rot.",
-    },
-  };
 
   const imageConstraints = {
     facingMode: "environment",
@@ -220,50 +142,55 @@ export default function UploadImage() {
               {moreDetails ? (
                 <div className="flex flex-col items-center justify-center gap-2 rounded-xl bg-green-100 p-4">
                   <p className="text-xl">
-                    <span className="text-2xl font-medium">Precautions: </span>
-                    {cropData[diseaseName].precautions}
+                    <span className="text-2xl font-medium">
+                      {t("precautionsLabel")}:{" "}
+                    </span>
+                    {t(`diseases.${diseaseName}.precautions`)}
                   </p>
                   <p className="text-xl">
                     <span className="text-2xl font-medium">
-                      Treatment/Care:{" "}
+                      {t("treatmentLabl")}
                     </span>
-                    {cropData[diseaseName].treatment}
+                    {t(`diseases.${diseaseName}.treatment`)}
                   </p>
                   <p className="text-xl">
                     <span className="text-2xl font-medium">
-                      Water Requirement:{" "}
+                      {t("waterRequirementLabel")}
                     </span>
-                    {cropData[diseaseName].water}
+                    {t(`diseases.${diseaseName}.water`)}
                   </p>
                   <div className="flex w-full items-center justify-between">
                     <button
                       onClick={() => setMoreDetails(false)}
                       className="bg-darkBrown mt-4 w-full rounded-lg py-2 text-white"
                     >
-                      Back
+                      {t("backButton")}
                     </button>
                   </div>
                 </div>
               ) : (
                 <div className="flex flex-col gap-2 rounded-xl bg-green-100 p-4">
                   <header className="text-3xl font-bold">
-                    Crop: {cropData[diseaseName].leafName}
+                    {t("cropLabel")}: {t(`diseases.${diseaseName}.leafName`)}
                   </header>
                   <p className="text-xl">
                     <span className="text-2xl font-medium">
-                      Status: {cropData[diseaseName].diseaseName}
+                      {t("statusLabel")}:{" "}
+                      {t(`diseases.${diseaseName}.diseaseName`)}
                     </span>
                   </p>
                   <p className="text-xl">
-                    <span className="text-2xl font-medium">Description: </span>
-                    {cropData[diseaseName].description}
+                    <span className="text-2xl font-medium">
+                      {t("descriptionLabel")}:{" "}
+                    </span>
+                    {t(`diseases.${diseaseName}.description`)}
                   </p>
                   <div className="flex items-center justify-between">
                     <button
                       className="bg-darkBrown mt-4 w-full rounded-lg py-2 text-white"
                       onClick={() => setMoreDetails(true)}
                     >
-                      More Details
+                      {t("moreDetailsButton")}
                     </button>
                   </div>
                 </div>
